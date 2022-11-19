5b33562dbf659316cfd6df606f0d9599d473abea
chemiofitor <783626185@qq.com>
2022-11-19 15:25:08 +0800

    3.2.0 pull

--- src/main/java/hualong/ticc/compat/TenTiCC.java
+++ src/main/java/hualong/ticc/compat/TenTiCC.java
@@ -19,10 +19,14 @@ public class TenTiCC {
 
     public static final RegistryObject<PhotosynthesisModifier> PHOTOSYNTHESIS = MODIFIERS.register("photosynthesis", PhotosynthesisModifier::new);
 
+    public static final FluidObject<ForgeFlowingFluid> GLOWSTONE = FLUIDS.register("glowstone", hotBuilder().temperature(200), Material.LAVA,  15);
+    public static final FluidObject<ForgeFlowingFluid> REDSTONE = FLUIDS.register("redstone", hotBuilder().temperature(700), Material.LAVA,  10);
     public static final FluidObject<ForgeFlowingFluid> MOLTEN_CHLORIUM = FLUIDS.register("molten_chlorium", hotBuilder().temperature(800), Material.LAVA,  14);
+    public static final FluidObject<ForgeFlowingFluid> MOLTEN_POWERED_TIN = FLUIDS.register("molten_powered_tin", hotBuilder().temperature(300), Material.LAVA,  14);
 
     public static final ItemObject<FrameBlock> CHLORIUM_FRAME = BLOCKS.register("chlorium_frame",
             FrameBlock::metal, block -> new BlockItem(block, itemPro()));
+
     private static FluidAttributes.Builder hotBuilder() {
         return ModelFluidAttributes.builder().density(2000).viscosity(10000).temperature(1000).sound(SoundEvents.BUCKET_FILL_LAVA, SoundEvents.BUCKET_EMPTY_LAVA);
     }
