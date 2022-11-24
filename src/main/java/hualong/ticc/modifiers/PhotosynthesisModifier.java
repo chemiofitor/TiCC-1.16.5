e899a5e65dd1f650ef36842f8fe2aa7b668c24d7
chemiofitor <783626185@qq.com>
2022-11-24 11:39:32 +0800

    3.4.1 pull

--- src/main/java/hualong/ticc/modifiers/PhotosynthesisModifier.java
+++ src/main/java/hualong/ticc/modifiers/PhotosynthesisModifier.java
@@ -2,6 +2,7 @@ package hualong.ticc.modifiers;
 
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import slimeknights.tconstruct.library.modifiers.Modifier;
 import slimeknights.tconstruct.library.tools.nbt.IModifierToolStack;
@@ -13,8 +14,9 @@ public class PhotosynthesisModifier extends Modifier {
 
     @Override
     public void onInventoryTick(IModifierToolStack tool, int level, World world, LivingEntity holder, int itemSlot, boolean isSelected, boolean isCorrectSlot, ItemStack stack) {
-        if (!world.isClientSide && holder.tickCount % 40 == 0 && holder.getUseItem() != stack) {
-            if (RANDOM.nextFloat() < 0.1 * level && (world.isRainingAt(holder.blockPosition().above()) || world.isDay())) {
+        if (!world.isClientSide && holder.tickCount % 20 == 0 && holder.getUseItem() != stack) {
+            BlockPos pos = holder.blockPosition().above();
+            if (RANDOM.nextFloat() < 0.15 * level && (world.isRainingAt(pos) || (world.isDay() && world.canSeeSky(pos)))) {
                 if (tool.getDamage() > 0)
                     tool.setDamage(tool.getDamage() - 1);
             }
